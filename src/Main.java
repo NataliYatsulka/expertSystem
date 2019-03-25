a0ab3223a974955a9bc90035602699334126d31a
nekitoss_main <nekit_p@ukr.net>
2019-03-26 00:26:49 +0200

    check bc()

--- src/Main.java
+++ src/Main.java
@@ -10,108 +10,99 @@ public class Main {
 //    public static List<String> rules =  Arrays.asList("(", ")", "!", "+", "|", "^", "=>", "<=>");
 //    public static String[] queries = new String[32];
 
-  public static HashMap<Character, Boolean> mapOfFacts = new HashMap<Character, Boolean>();
-  public static HashMap<Character, Integer> mapOfWords = new HashMap<Character, Integer>();
-  public static ArrayList<Character> facts = new ArrayList<>();
-  public static ArrayList<Character> queries = new ArrayList<>();
-  public static ArrayList<String> leftPart = new ArrayList<>();
-  public static ArrayList<String> rightPart = new ArrayList<>();
-  public static Stack<String> table = new Stack<>();
-  public static Stack<String> tableRight = new Stack<>();
-  public static int countRaw;
-  public static Integer[][] tableGrid;
+    public static HashMap<Character, Boolean> mapOfFacts = new HashMap<Character, Boolean>();
+    public static HashMap<Character, Integer> mapOfWords = new HashMap<Character, Integer>();
+    public static ArrayList<Character> facts = new ArrayList<>();
+    public static ArrayList<Character> queries = new ArrayList<>();
+    public static ArrayList<String> leftPart = new ArrayList<>();
+    public static ArrayList<String> rightPart = new ArrayList<>();
+    public static Stack<String> table = new Stack<>();
+    public static Stack<String> tableRight = new Stack<>();
+    public static int countRaw;
+    public static Integer[][] tableGrid;
 //  public static boolean[] beingInThisRaw;
 
-  public static void outputRes() {
-    System.out.println("\u001B[32m" + "The Result is next: " + "\u001B[0m");
-    for (int i = 0; i < queries.size(); i++) {
-      System.out.println("\u001B[32m" + queries.get(i) + " = " + mapOfFacts.get(queries.get(i)) + "\u001B[0m");
-    }
-  }
-
-  public static void main(String[] args) {
-    String path = null;
-    Options options = Parser.parsingInputArgs(args);
-    List<String> list;
-
-    HelpFormatter formatter = new HelpFormatter();
-facts.contains(queries);
-    try {
-      if (args.length < 2)
-        Parser.usage(formatter, options);
-      CommandLine cmd = new BasicParser().parse(options, args);
-      path = cmd.getOptionValue("path");
-      System.out.println("path = " + path);
-      if (path != null) {
-        File f = new File(path);
-        if (!f.exists() || f.isDirectory()) {
-          throw new FileNotFoundException("ERROR:   File not found");
+    public static void outputRes() {
+        System.out.println("\u001B[32m" + "The Result is next: " + "\u001B[0m");
+        for (int i = 0; i < queries.size(); i++) {
+            System.out.println("\u001B[32m" + queries.get(i) + " = " + mapOfFacts.get(queries.get(i)) + "\u001B[0m");
         }
-        list = Parser.readTextFileByLines(path);
-        if (!list.isEmpty())
-          Parser.deleteComments(list);
-        else
-          Message.exception("ERROR:  The list is empty");
-        Parser.checkFileOnFactQueries(list);
-        Parser.checkFile(list);
+    }
 
+    public static void main(String[] args) {
+        String path = null;
+        Options options = Parser.parsingInputArgs(args);
+        List<String> list;
+
+        HelpFormatter formatter = new HelpFormatter();
+        facts.contains(queries);
+        try {
+            if (args.length < 2)
+                Parser.usage(formatter, options);
+            CommandLine cmd = new BasicParser().parse(options, args);
+            path = cmd.getOptionValue("path");
+            System.out.println("path = " + path);
+            if (path != null) {
+                File f = new File(path);
+                if (!f.exists() || f.isDirectory()) {
+                    throw new FileNotFoundException("ERROR:   File not found");
+                }
+                list = Parser.readTextFileByLines(path);
+                if (!list.isEmpty())
+                    Parser.deleteComments(list);
+                else
+                    Message.exception("ERROR:  The list is empty");
+                Parser.checkFileOnFactQueries(list);
+                Parser.checkFile(list);
+
+            }
+        } catch (ParseException | IOException ex) {
+            System.out.println(ex.getMessage());
+            Parser.usage(formatter, options);
+        }
+
+        for (int i = 0; i < leftPart.size(); i++) {
+            table.push(Algo.parse(leftPart.get(i)).toString());
+            tableRight.push(Algo.parse(rightPart.get(i)).toString());
+            System.out.println("table = " + table.get(i));
+            System.out.println("tableRight = " + tableRight.get(i));
+
+        }
+
+        tableGrid = new Integer[Main.countRaw][26];
+        System.out.println("leftPart = " + leftPart);
+
+        for (Map.Entry<Character, Boolean> oneEntry : mapOfFacts.entrySet()) {
+            for (int j = 0; j < countRaw; j++) {
+                tableGrid[j][(oneEntry.getKey() - 'A')] = 0;
+                if ((leftPart.get(j)).indexOf(oneEntry.getKey()) >= 0) {
+                    tableGrid[j][(oneEntry.getKey() - 'A')] = 1;
+                }
+                if ((rightPart.get(j)).indexOf(oneEntry.getKey()) >= 0) {
+                    tableGrid[j][(oneEntry.getKey() - 'A')] = 2;
+                }
+            }
+        }
+
+        for (int i = 0; i < Main.countRaw; i++) {
+            for (int j = 0; j < 26; j++) {
+                if (tableGrid[i][j] == null)
+                    tableGrid[i][j] = 0;
+            }
+        }
+
+        //delete
+        for (int i = 0; i < Main.countRaw; i++) {
+            for (int j = 0; j < 26; j++) {
+                System.out.print(String.format("%3d", tableGrid[i][j]));
+            }
+            System.out.println();
+        }
+
+        for (Character query : queries) {
+            Parser.bc(query);
+        }
         outputRes();
-      }
-    } catch (ParseException | IOException ex) {
-      System.out.println(ex.getMessage());
-      Parser.usage(formatter, options);
+        System.out.println("End!");
     }
-
-    for (int i = 0; i < leftPart.size(); i++) {
-      table.push(Algo.parse(leftPart.get(i)).toString());
-      tableRight.push(Algo.parse(rightPart.get(i)).toString());
-      System.out.println("table = " + table.get(i));
-      System.out.println("tableRight = " + tableRight.get(i));
-
-    }
-
-//      for (int i = 'A'; i <= 'Z'; i++) {
-//          mapOfWords.put((char)i, i - 'A');
-//      }
-      System.out.println(mapOfWords.get('D'));
-
-    tableGrid = new Integer[Main.countRaw][26];
-      System.out.println("leftPart = " + leftPart);
-
-//    char - 'A'
-
-    for (Map.Entry<Character, Boolean> oneEntry : mapOfFacts.entrySet()){
-        for (int j = 0; j < countRaw; j++){
-            tableGrid[j][(oneEntry.getKey() - 'A')] = 0;
-            if ((leftPart.get(j)).indexOf(oneEntry.getKey()) >= 0){
-                tableGrid[j][(oneEntry.getKey() - 'A')] = 1;
-            }
-            if ((rightPart.get(j)).indexOf(oneEntry.getKey()) >= 0){
-                tableGrid[j][(oneEntry.getKey() - 'A')] = 2;
-            }
-        }
-    }
-
-    //delete
-    for (int i = 0; i < Main.countRaw; i++)
-    {
-        for (int j = 0; j < 26; j++){
-            System.out.print(String.format("%5d", tableGrid[i][j]));
-        }
-        System.out.println();
-    }
-
-//      System.out.println("tableGrid = " + Arrays.deepToString(tableGrid));
-
-//    try {
-//      Parser.solve();
-//    } catch (IOException ex) {
-//      System.out.println(ex.getMessage());
-//      Parser.usage(formatter, options);
-//    }
-      for (Character query: queries) {
-          Parser.bc(query);
-      }
-    System.out.println("End!");
-  }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -8,223 +8,196 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 public class Parser {
-  public static int[][] mas;
+    public static int[][] mas;
 
-  public static Options parsingInputArgs(String[] args) {
-    Options options = new Options();
+    public static Options parsingInputArgs(String[] args) {
+        Options options = new Options();
 
-    Option path = new Option("p", "path", true, "path to your file");
-    path.setRequired(false);
-    options.addOption(path);
+        Option path = new Option("p", "path", true, "path to your file");
+        path.setRequired(false);
+        options.addOption(path);
 
-    return options;
-  }
-
-  public static void usage(HelpFormatter formatter, Options options) {
-    formatter.printHelp("expert-system", options);
-    System.exit(1);
-  }
-
-  public static List<String> readTextFileByLines(String fileName) throws IOException {
-    return Files.readAllLines(Paths.get(fileName));
-  }
-
-  public static void deleteComments(List<String> list) {
-
-    for (int i = 0; i < list.size(); i++) {
-      String[] res = list.get(i).split("#");
-      list.set(i, res[0]);
-      if (list.get(i).isEmpty()) {
-        list.remove(i);
-        i--;
-      }
+        return options;
     }
 
-    for (int i = 0; i < list.size(); i++) {
-      String[] res = list.get(i).split(" ");
-      String row = "";
-      for (String re : res) {
-        row = row + re;
-      }
-      list.set(i, row);
+    public static void usage(HelpFormatter formatter, Options options) {
+        formatter.printHelp("expert-system", options);
+        System.exit(1);
+    }
+
+    public static List<String> readTextFileByLines(String fileName) throws IOException {
+        return Files.readAllLines(Paths.get(fileName));
+    }
+
+    public static void deleteComments(List<String> list) {
+
+        for (int i = 0; i < list.size(); i++) {
+            String[] res = list.get(i).split("#");
+            list.set(i, res[0]);
+            if (list.get(i).isEmpty()) {
+                list.remove(i);
+                i--;
+            }
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            String[] res = list.get(i).split(" ");
+            String row = "";
+            for (String re : res) {
+                row = row + re;
+            }
+            list.set(i, row);
 //      System.out.println("1 " + list.get(i));
+        }
     }
-  }
 
 
-  public static void checkFileOnFactQueries(List<String> list) {
-    int count = 0;
+    public static void checkFileOnFactQueries(List<String> list) {
+        int count = 0;
 //    int countRaw = 0;
-    for (int i = 0; i < list.size(); i++) {
-      if (list.get(i).matches("=[A-Z]+")) {
-        String[] s = list.get(i).split("=");
-        for (int k = 0; k < s[1].length(); k++)
-          Main.facts.add(s[1].charAt(k));
-        list.remove(i);
-        i--;
-        count++;
-      }
-      if (list.get(i).matches("\\?[A-Z]+")) {
-        String[] s = list.get(i).split("\\?");
-        for (int k = 0; k < s[1].length(); k++)
-          Main.queries.add(s[1].charAt(k));
-        list.remove(i);
-        i--;
-        count++;
-      }
-      Main.countRaw = i;
-    }
-    Main.countRaw++;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).matches("=[A-Z]+")) {
+                String[] s = list.get(i).split("=");
+                for (int k = 0; k < s[1].length(); k++)
+                    Main.facts.add(s[1].charAt(k));
+                list.remove(i);
+                i--;
+                count++;
+            }
+            if (list.get(i).matches("\\?[A-Z]+")) {
+                String[] s = list.get(i).split("\\?");
+                for (int k = 0; k < s[1].length(); k++)
+                    Main.queries.add(s[1].charAt(k));
+                list.remove(i);
+                i--;
+                count++;
+            }
+            Main.countRaw = i;
+        }
+        Main.countRaw++;
 //    Main.beingInThisRaw = new boolean[++countRaw];
 //    System.out.println("booleanMass   = " + Main.beingInThisRaw);
-    System.out.println("ooo =" + list);
+        System.out.println("ooo =" + list);
 //    Main.countRaw = countRaw;
-    System.out.println("Main.countRaw = " + Main.countRaw);
-    System.out.println(Main.facts + "     " + Main.queries);
-    if (count != 2)
-      Message.exception("ERROR:  more than one time = or ?");
-  }
+        System.out.println("Main.countRaw = " + Main.countRaw);
+        System.out.println(Main.facts + "     " + Main.queries);
+        if (count != 2)
+            Message.exception("ERROR:  more than one time = or ?");
+    }
 
-  public static boolean isLetterOrSign(char c) {
-    return (c >= 'A' && c <= 'Z') || c == '(' || c == ')' || c == '+' || c == '^' || c == '|' || c == '!';
-  }
+    public static boolean isLetterOrSign(char c) {
+        return (c >= 'A' && c <= 'Z') || c == '(' || c == ')' || c == '+' || c == '^' || c == '|' || c == '!';
+    }
 
-  public static void checkLeftRightSiteOfRow(ArrayList<String> left) {
+    public static void checkLeftRightSiteOfRow(ArrayList<String> left) {
 
-    for (int i = 0; i < left.size(); i++) {
-      for (int j = 0; j < left.get(i).length(); j++) {
-        if (!isLetterOrSign(left.get(i).charAt(j)))
-          Message.exception("ERROR:  Not right row " + i);
-        char c = left.get(i).charAt(j);
-        char prev = c;
+        for (int i = 0; i < left.size(); i++) {
+            for (int j = 0; j < left.get(i).length(); j++) {
+                if (!isLetterOrSign(left.get(i).charAt(j)))
+                    Message.exception("ERROR:  Not right row " + i);
+                char c = left.get(i).charAt(j);
+                char prev = c;
 
-        if (Character.isLetter(prev))
-          Main.mapOfFacts.put(prev, false);
+                if (Character.isLetter(prev))
+                    Main.mapOfFacts.put(prev, false);
 
-        if (j == 0 && left.get(i).length() == 1) {
-          if (!Character.isLetter(prev))
-            Message.exception("ERROR:  Row can be only A-Z in row " + i);
-        } else if (j != left.get(i).length() - 1) {
+                if (j == 0 && left.get(i).length() == 1) {
+                    if (!Character.isLetter(prev))
+                        Message.exception("ERROR:  Row can be only A-Z in row " + i);
+                } else if (j != left.get(i).length() - 1) {
 
-          char next = left.get(i).charAt(j + 1);
+                    char next = left.get(i).charAt(j + 1);
 
-          if (j == 0 && (c == ')' || c == '+' || c == '^' || c == '|')) {
-            Message.exception("ERROR:  Row can not start by " + c);
-          } else if (j == left.get(i).length() - 1 && (c == '(' || c == '+' || c == '^' || c == '|')) {
-            Message.exception("ERROR:  Row can not end " + c);
-          } else if ((prev == '!' && (next == '+' || next == '^' || next == '|' || next == '!'))
-                  || (prev == '(' && (next == '+' || next == '^' || next == '|' || next == ')'))
-                  || ((Character.isLetter(prev) || prev == ')') && (next == '(' || Character.isLetter(next) || next == '!'))
-                  || ((prev == '+' || prev == '^' || prev == '|') && (next == '+' || next == '^' || next == '|' || next == ')')))
-            Message.exception("ERROR:  Row can not have after " + prev + " - " + next);
+                    if (j == 0 && (c == ')' || c == '+' || c == '^' || c == '|')) {
+                        Message.exception("ERROR:  Row can not start by " + c);
+                    } else if (j == left.get(i).length() - 1 && (c == '(' || c == '+' || c == '^' || c == '|')) {
+                        Message.exception("ERROR:  Row can not end " + c);
+                    } else if ((prev == '!' && (next == '+' || next == '^' || next == '|' || next == '!'))
+                            || (prev == '(' && (next == '+' || next == '^' || next == '|' || next == ')'))
+                            || ((Character.isLetter(prev) || prev == ')') && (next == '(' || Character.isLetter(next) || next == '!'))
+                            || ((prev == '+' || prev == '^' || prev == '|') && (next == '+' || next == '^' || next == '|' || next == ')')))
+                        Message.exception("ERROR:  Row can not have after " + prev + " - " + next);
+                }
+            }
         }
-      }
-    }
-  }
-
-  public static void addFactsToMap() {
-      for (int i = 0; i < Main.facts.size(); i++) {
-      Main.mapOfFacts.put(Main.facts.get(i), true);
-    }
-  }
-
-  public static boolean checkFile(List<String> list) {
-    List<Structure> structures = new ArrayList<>();
-    for (int i = 0; i < list.size(); i++) {
-      String[] tmp = list.get(i).split("<=>");
-      if (list.get(i).matches(".+<=>.+")) {
-        structures.add(new Structure(tmp[0], tmp[1], true, false));
-      } else if (list.get(i).matches(".+=>.+")) {
-        tmp = list.get(i).split("=>");
-        structures.add(new Structure(tmp[0], tmp[1], false, false));
-      } else
-        Message.exception("ERROR:  One of the line have bad initialization in row  " + list.get(i));
-      Main.leftPart.add(tmp[0]);
-      Main.rightPart.add(tmp[1]);
-      if ((StringUtils.countMatches(tmp[0], "(") != StringUtils.countMatches(tmp[0], ")")) ||
-              (StringUtils.countMatches(tmp[1], "(") != StringUtils.countMatches(tmp[1], ")")))
-        Message.exception("ERROR:  You forgot () in the row");
-    }
-    checkLeftRightSiteOfRow(Main.leftPart);
-    checkLeftRightSiteOfRow(Main.rightPart);
-    addFactsToMap();
-    System.out.println("mapOfFacts = " + Main.mapOfFacts);
-    System.out.println("facts =" + Main.facts);
-    for (int z = 0; z < structures.size(); z++) {
-      System.out.println("qqq    = " + structures.get(z).toString());
     }
 
-//    try {
-//      Parser.bc();
-//    } catch (IOException ex) {
- //     System.out.println(ex.getMessage());
-     // Message.exception("OPPSSS...");
-   // }
+    public static void addFactsToMap() {
+        for (int i = 0; i < Main.facts.size(); i++) {
+            Main.mapOfFacts.put(Main.facts.get(i), true);
+        }
+    }
 
-    return true;
-  }
+    public static boolean checkFile(List<String> list) {
+        List<Structure> structures = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            String[] tmp = list.get(i).split("<=>");
+            if (list.get(i).matches(".+<=>.+")) {
+                structures.add(new Structure(tmp[0], tmp[1], true, false));
+            } else if (list.get(i).matches(".+=>.+")) {
+                tmp = list.get(i).split("=>");
+                structures.add(new Structure(tmp[0], tmp[1], false, false));
+            } else
+                Message.exception("ERROR:  One of the line have bad initialization in row  " + list.get(i));
+            Main.leftPart.add(tmp[0]);
+            Main.rightPart.add(tmp[1]);
+            if ((StringUtils.countMatches(tmp[0], "(") != StringUtils.countMatches(tmp[0], ")")) ||
+                    (StringUtils.countMatches(tmp[1], "(") != StringUtils.countMatches(tmp[1], ")")))
+                Message.exception("ERROR:  You forgot () in the row");
+        }
+        checkLeftRightSiteOfRow(Main.leftPart);
+        checkLeftRightSiteOfRow(Main.rightPart);
+        addFactsToMap();
+        System.out.println("mapOfFacts = " + Main.mapOfFacts);
+        System.out.println("facts =" + Main.facts);
+        for (int z = 0; z < structures.size(); z++) {
+            System.out.println("qqq    = " + structures.get(z).toString());
+        }
+        return true;
+    }
 
-  public static Integer[] findAllRulles(int idexQuery){
-      Integer[] mas = new Integer[Main.countRaw];
-      for (int i = 0; i < Main.countRaw; i++){
-          if (Main.tableGrid[i][idexQuery] == 2) {
-              mas[i] = i;
-          }
-      }
-      return mas;
-  }
 
-    public static Integer[] findAllCondition(int idexQuery){
-        Integer[] mas = new Integer[Main.countRaw];
-        for (int i = 0; i < Main.countRaw; i++){
-            if (Main.tableGrid[i][idexQuery] == 1) {
-                mas[i] = i;
+    public static ArrayList<Integer> findAllQuery(int indexQuery) {
+        ArrayList<Integer> mas = new ArrayList<>();
+        for (int i = 0; i < Main.countRaw; i++) {
+            if (Main.tableGrid[i][indexQuery] == 2) {
+                mas.add(i);
             }
         }
         return mas;
     }
 
-//    public static ArrayList<Integer> findAllCondition(int idexQuery){
-//        ArrayList<Integer> mas = new ArrayList<>();
-//        for (int i = 0; i < Main.countRaw; i++){
-//            if (Main.tableGrid[i][idexQuery] == 1) {
-//                mas.add(i);
-//            }
-//        }
-//        return mas;
-//    }
+    public static ArrayList<Integer> findAllCondition(int indexQuery) {
+        ArrayList<Integer> mas = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            if (Main.tableGrid[indexQuery][i] == 1) {
+                mas.add(i);
+            }
+        }
+        return mas;
+    }
 
-  public static void bc(char query) {// throws IOException {
-//    Stack<Boolean> stack = new Stack<>();
-//    while (!Main.table.isEmpty()) {
-//      Character c;
-//      if (Main.facts.contains(c)) {
-//        stack.add(true);
-//      }
-//    }
-      Integer[] masQuery;// = new ArrayList<>();
-      Integer[] masCondit;
-//      for (Character query: Main.queries){
-          if (!Main.facts.contains(query)){
-              System.out.println("\u001B[33m" + "Querie to evaluete: " + query + "\u001B[0m");
-              int indexQuery = query - 'A';
-              masQuery = findAllRulles(indexQuery);
-              if (masQuery.length < 1){
-                  Main.mapOfFacts.put(query, false);
-              }
-              for (int j = 0; j < masQuery.length; j++){
-                  masCondit = findAllCondition(masQuery[j]);
-                  for (int k = 0; k < masCondit.length; k++){
-                      bc(masCondit[k] - 'A');
-                  }
+    public static void bc(char query) {// throws IOException {
+        ArrayList<Integer> masQuery = new ArrayList<>();
+        ArrayList<Integer> masCondit = new ArrayList<>();
 
-
-
-              }
-          }
-//      }
-  }
+        if (!Main.facts.contains(query)) {
+            System.out.println("\u001B[33m" + "Querie to evaluete: " + query + "\u001B[0m");
+            int indexQuery = query - 'A';
+            masQuery = findAllQuery(indexQuery);
+            System.out.println("masQuery " + masQuery.size());
+            if (masQuery.size() < 1) {
+                Main.mapOfFacts.put(query, false);
+            }
+            for (int j = 0; j < masQuery.size(); j++) {
+                masCondit = findAllCondition(masQuery.get(j));
+                System.out.println("masCond" + masCondit);
+                for (int k = 0; k < masCondit.size(); k++) {
+                    bc((char) (masCondit.get(k) + 'A'));
+                }
+            }
+        }
+    }
 }
